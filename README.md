11e86d611924a7a475b95af8615fb15962107d63
chuangfe <chuangfe@gmail.com>
2021-07-14 09:54:30 +0800

    Update 調整樣式.

--- README.md
+++ README.md
@@ -18,6 +18,4 @@ https://chuangfe.github.io/product-gallery/
 
 ## Copyright©
 
-設計稿參考
-
-https://hexschool.github.io/THE_F2E_Design/week4-product%20gallery/
+設計稿參考 https://hexschool.github.io/THE_F2E_Design/week4-product%20gallery/
---
+++ dist/script/script.js
@@ -0,0 +1,45 @@
+(function () {
+  const vm = new Vue({
+    el: "#app",
+    data: {
+      products: [
+        {
+          title: "CAROUSEL",
+          text: "jQuery",
+          imageSrc: "./images/carousel-01.jpg",
+          githubSrc: "https://github.com/chuangfe/carousel",
+          githubPagesSrc: "https://chuangfe.github.io/carousel/dist",
+        },
+        {
+          title: "CAROUSEL",
+          text: "VUE",
+          imageSrc: "./images/carousel-02.png",
+          githubSrc: "https://github.com/chuangfe/carousel-vue",
+          githubPagesSrc: "https://chuangfe.github.io/carousel-vue/dist",
+        },
+        {
+          title: "MASK MAP",
+          text: "VUE / LEAFLET",
+          imageSrc: "./images/mask-map.png",
+          githubSrc: "https://github.com/chuangfe/mask-map",
+          githubPagesSrc: "https://chuangfe.github.io/mask-map/dist",
+        },
+        {
+          title: "TODO MVC",
+          text: "VUE",
+          imageSrc: "./images/todo-mvc.png",
+          githubSrc: "https://github.com/chuangfe/todomvc-vue",
+          githubPagesSrc: "https://chuangfe.github.io/todomvc-vue",
+        },
+      ],
+    },
+    methods: {
+      getNumberPadStart(i) {
+        return String(i).padStart(2, "0");
+      },
+      getItemClass(i) {
+        return "item-" + i;
+      },
+    },
+  });
+})();
